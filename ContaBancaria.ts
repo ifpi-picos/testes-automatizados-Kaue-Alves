13e7921399b5932e703c4108104f6ebff9366874
Kaue-Alves <akaue325@gmail.com>
2024-09-16 01:20:56 -0300

    transferir quase completo

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -4,6 +4,14 @@ export default class ContaBancaria {
   private saldo: number = 0
   private extrato: string[] // Armazena o histórico de operações realizadas na conta (depósitos, saques, transferências).
   
+
+  public constructor() {
+    this.numeroConta = 0
+    this.agencia = 1
+    this.saldo = 0
+    this.extrato = []
+  }
+  
   // 1. **depositar(valor: number)**: Permite realizar um depósito na conta, aumentando o saldo.
 
   public depositar(valor: number) {
@@ -27,9 +35,17 @@ export default class ContaBancaria {
   // 4. **consultarSaldo()**: Retorna o saldo atual da conta.
   
   public consultarSaldo() {
+    console.log(`Saldo atual da conta: R$ ${this.saldo}`);
+    
     return `Saldo atual da conta: R$ ${this.saldo}`
   }
   
+  public transferir(valor: number, contaDestino: ContaBancaria) {
+    if (valor > 0 && valor <= this.saldo) {
+      this.saldo -= valor
+      contaDestino.depositar(valor)
+    }
+  }
   
 /*
 3. **transferir(valor: number, contaDestino: ContaBancaria)**: Realiza a transferência de um valor para outra conta bancária, diminuindo o saldo da conta origem e aumentando o saldo da conta destino.
@@ -38,3 +54,15 @@ export default class ContaBancaria {
 */
   
 }
+
+let conta1 = new ContaBancaria()
+let conta2 = new ContaBancaria()
+
+conta1.depositar(200)
+conta1.consultarSaldo()
+conta2.consultarSaldo()
+
+conta1.transferir(200, conta2)
+
+conta1.consultarSaldo()
+conta2.consultarSaldo()
