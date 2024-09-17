305318704b3fc19f4ff030c536d5830a6d441699
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-09-17 10:22:16 -0300

    Depositar e Sacar - Testes Depositar e Sacar

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -2,15 +2,27 @@ import { beforeEach, describe, expect, test } from "bun:test";
 
 import ContaBancaria from "./ContaBancaria.ts";
 
-describe ("Teste da classe ContaBancaria", () => {
-  
+describe("Teste da classe ContaBancaria", () => {
   let conta: ContaBancaria
   beforeEach(() => {
     conta = new ContaBancaria()
   })
-  
-  test("Teste da funcão depositar", () => {
-    conta.depositar(500)
-    expect(conta.consultarSaldo()).toBe(`Saldo atual da conta: R$ 500`)
+
+  test("Método Depositar", () => {
+    expect(conta.depositar(200)).toBe(200)
   })
+
+  test("Método Depositar - Depósito Inválido", () => {
+    expect(() => conta.depositar(-100)).toThrow("Valor inválido")
+  })
+
+  test("Método Sacar", () => {
+    conta.depositar(300)
+    expect(conta.sacar(200)).toBe(100)
+  })
+  
+  test("Método Sacar - Saque Inválido", () => {
+    expect(() => expect(conta.sacar(-80)).toThrow("Valor inválido ou saldo insuficiente"))
+  })
+
 })
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,51 +1,59 @@
 export default class ContaBancaria {
   private numeroConta: number
   private agencia: number
-  private saldo: number = 0
+  private saldo: number
   private extrato: string[] // Armazena o histórico de operações realizadas na conta (depósitos, saques, transferências).
-  
 
   public constructor() {
     this.numeroConta = 0
-    this.agencia = 1
+    this.agencia = 0
     this.saldo = 0
     this.extrato = []
   }
-  
-  // 1. **depositar(valor: number)**: Permite realizar um depósito na conta, aumentando o saldo.
 
   public depositar(valor: number) {
-    if (valor) {
-      return this.saldo += valor
+    if (valor > 0) {
+      this.saldo += valor
+      this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`)
+      return this.saldo
     } else {
-      return "O valor do depósito não pode ser nulo ou negativo."
+      throw new Error("Valor inválido");
     }
   }
-  
-  // 2. **sacar(valor: number)**: Permite realizar um saque, diminuindo o saldo, desde que o valor seja válido e não exceda o saldo disponível.
-  
+
   public sacar(valor: number) {
-    if (valor) {
-      return this.saldo -= valor
-    } else {
-      return "O valor do saque não pode ser nulo ou negativo."
-    }
-  }
-  
-  // 4. **consultarSaldo()**: Retorna o saldo atual da conta.
-  
-  public consultarSaldo() {
-    console.log(`Saldo atual da conta: R$ ${this.saldo}`);
-    
-    return `Saldo atual da conta: R$ ${this.saldo}`
-  }
-  
-  public transferir(valor: number, contaDestino: ContaBancaria) {
     if (valor > 0 && valor <= this.saldo) {
       this.saldo -= valor
-      contaDestino.depositar(valor)
+      this.extrato.push(`Saque de R$ ${valor.toFixed(2)}`)
+      return this.saldo
+    } else {
+      throw new Error("Valor inválido ou saldo insuficiente");
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+  
+  
+  // 1. **depositar(valor: number)**: Permite realizar um depósito na conta, aumentando o saldo.
+  // 2. **sacar(valor: number)**: Permite realizar um saque, diminuindo o saldo, desde que o valor seja válido e não exceda o saldo disponível.
+
+  // 4. **consultarSaldo()**: Retorna o saldo atual da conta.
   
 /*
 3. **transferir(valor: number, contaDestino: ContaBancaria)**: Realiza a transferência de um valor para outra conta bancária, diminuindo o saldo da conta origem e aumentando o saldo da conta destino.
@@ -54,15 +62,3 @@ export default class ContaBancaria {
 */
   
 }
-
-let conta1 = new ContaBancaria()
-let conta2 = new ContaBancaria()
-
-conta1.depositar(200)
-conta1.consultarSaldo()
-conta2.consultarSaldo()
-
-conta1.transferir(200, conta2)
-
-conta1.consultarSaldo()
-conta2.consultarSaldo()
