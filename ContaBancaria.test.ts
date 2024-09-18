82876258a1b311d748963ad62253c86586b6d342
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-09-18 16:40:53 -0300

    Transferir - Teste Transferir

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -4,8 +4,10 @@ import ContaBancaria from "./ContaBancaria.ts";
 
 describe("Teste da classe ContaBancaria", () => {
   let conta: ContaBancaria
+  let contaDois: ContaBancaria
   beforeEach(() => {
     conta = new ContaBancaria()
+    contaDois = new ContaBancaria()
   })
 
   test("Método Depositar", () => {
@@ -22,7 +24,16 @@ describe("Teste da classe ContaBancaria", () => {
   })
   
   test("Método Sacar - Saque Inválido", () => {
-    expect(() => expect(conta.sacar(-80)).toThrow("Valor inválido ou saldo insuficiente"))
+    expect(() => conta.sacar(-80)).toThrow("Valor inválido ou saldo insuficiente")
   })
 
+  test("Método transferir", () => {
+    conta.depositar(500)
+    expect(conta.transferir(200, contaDois)).toBe(300)
+  })
+
+  test("Método transferir - Valor Inválido", () => {
+    expect(() => conta.transferir(200, contaDois)).toThrow("Valor inválido ou saldo insuficiente")
+  })
+  
 })
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,21 +1,21 @@
 export default class ContaBancaria {
-  private numeroConta: number
-  private agencia: number
-  private saldo: number
-  private extrato: string[] // Armazena o histórico de operações realizadas na conta (depósitos, saques, transferências).
+  private numeroConta: number;
+  private agencia: number;
+  private saldo: number;
+  private extrato: string[]; // Armazena o histórico de operações realizadas na conta (depósitos, saques, transferências).
 
   public constructor() {
-    this.numeroConta = 0
-    this.agencia = 0
-    this.saldo = 0
-    this.extrato = []
+    this.numeroConta = 0;
+    this.agencia = 0;
+    this.saldo = 0;
+    this.extrato = [];
   }
 
   public depositar(valor: number) {
     if (valor > 0) {
-      this.saldo += valor
-      this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`)
-      return this.saldo
+      this.saldo += valor;
+      this.extrato.push(`Depósito de R$ ${valor.toFixed(2)}`);
+      return this.saldo;
     } else {
       throw new Error("Valor inválido");
     }
@@ -23,42 +23,42 @@ export default class ContaBancaria {
 
   public sacar(valor: number) {
     if (valor > 0 && valor <= this.saldo) {
-      this.saldo -= valor
-      this.extrato.push(`Saque de R$ ${valor.toFixed(2)}`)
-      return this.saldo
+      this.saldo -= valor;
+      this.extrato.push(`Saque de R$ ${valor.toFixed(2)}`);
+      return this.saldo;
     } else {
       throw new Error("Valor inválido ou saldo insuficiente");
     }
   }
 
+  private receberTransferencia(valor: number, conta: ContaBancaria) {
+    conta.saldo += valor
+    conta.extrato.push(`Transferência de R$ ${valor} recebida`)
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
-  
-  
-  // 1. **depositar(valor: number)**: Permite realizar um depósito na conta, aumentando o saldo.
-  // 2. **sacar(valor: number)**: Permite realizar um saque, diminuindo o saldo, desde que o valor seja válido e não exceda o saldo disponível.
+  public transferir(valor: number, conta: ContaBancaria) {
+    if (valor > 0 && valor <= this.saldo) {
+      conta.receberTransferencia(valor, conta)
+      this.saldo -= valor
+      this.extrato.push(`Transferência de R$ ${valor} realizada.`)
+      return this.saldo;
+    } else {
+      throw new Error("Valor inválido ou saldo insuficiente");
+    }
+  }
 
   // 4. **consultarSaldo()**: Retorna o saldo atual da conta.
-  
-/*
+
+  /*
 3. **transferir(valor: number, contaDestino: ContaBancaria)**: Realiza a transferência de um valor para outra conta bancária, diminuindo o saldo da conta origem e aumentando o saldo da conta destino.
 5. **exibirExtrato()**: Exibe o histórico de transações (extrato) realizadas na conta.
 6. **registrarOperacao(descricao: string)**: Método privado para registrar cada operação no extrato da conta, incluindo a data e a descrição da transação.
 */
-  
 }
+
+let conta = new ContaBancaria
+let contaDois = new ContaBancaria
+
+conta.depositar(500)
+
+conta.transferir(200, contaDois)
